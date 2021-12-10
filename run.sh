0a354deaf5cd57f169c9516ce8a5a3eb90826315
Bouke Steemers <bouke@bluezebra.io>
2021-12-10 15:32:13 +0100

    [FIX] https -> ssh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/bluezebra-io/odoo-14-docker-compose $DESTINATION
+git clone --depth=1 git@github.com:bluezebra-io/odoo-14-docker-compose.git $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
 sudo chmod -R 777 $DESTINATION
 # enterprise
 cd $DESTINATION
-git clone -b 14.0 https://github.com/odoo/enterprise.git
+git clone -b 14.0 git@github.com:odoo/enterprise.git
 cd ..
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
